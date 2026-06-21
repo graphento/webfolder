75a722c14e3507532f2484d8a2f2f6125b7e0d87
graphento <idc>
2026-06-21 21:53:32 +0500

    better utils

--- frontend/shared/scripts/utils.js
+++ frontend/shared/scripts/utils.js
@@ -1,18 +1,6 @@
 // JS utils
 
 /**
- * @template T
- * @param {Set<T>} set
- * @param {Iterable<T>} items
- */
-export function setHasAll(set, items) {
-  for (const item of items) {
-    if (!set.has(item)) return false;
-  }
-  return true;
-}
-
-/**
  * Creates element by tag, handling namespaced as well
  * @template {keyof HTMLElementTagNameMap} K
  * @param {K} tag
@@ -26,46 +14,59 @@ export function createElement(tag) {
 }
 
 /**
- * @template T
+ * Simple iterable weakarray. Cleans up on length query or iterating
+ * @template U
+ * @template {NonNullable<U>} T
  */
-export class IterableWeakSet {
-  /** @type {Set<WeakRef<T>>} */
-  #set = new Set();
+export class WeakArray {
+  /** @type {WeakRef<T>[]} */
+  #array;
 
   /**
    * @param {Iterable<T>} iterable
    */
   constructor(iterable = []) {
-    this.insert(...iterable);
+    this.#array = [...iterable];
   }
 
   /**
-   * Adds items to the set
    * @param {...T} items
-   * 
    */
-  insert(...items) {
+  push(...items) {
     for (const item of items) {
-      this.#set.add(new WeakRef(item));
+      this.#array.push(new WeakRef(item));
     }
+  }
+
+  /**
+   * @param {T} item
+   */
+  has(item) {
+    return this.#array.some((ref) => ref.deref() === item);
   }
 
   /**
    * @param {...T} items
    */
-  delete(...items) {
+  remove(...items) {
     const itemsSet = new Set(items);
-    for (const item of this.#set) {
-      const deref = item.deref();
-      if (!deref || itemsSet.has(deref)) this.#set.delete(item);
-    }
+    this.#array = this.#array.filter((item) => !itemsSet.has(item.deref()));
+  }
+
+  get len() {
+    this.#cleanup();
+    return this.#array.length;
   }
 
   *[Symbol.iterator]() {
-    for (const item of this.#set) {
+    this.#cleanup();
+    for (const item of [...this.#array]) {
       const deref = item.deref();
-      if (!deref) this.#set.delete(item);
-      else yield deref;
+      if (deref) yield deref;
     }
+  }
+
+  #cleanup() {
+    this.#array = this.#array.filter((item) => item.deref());
   }
 }
